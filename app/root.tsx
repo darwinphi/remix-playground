5365243cb9e31175537c0118b9193bbafe9b066c
Darwin Manalo <darwinmanalophi@gmail.com>
2022-02-11 20:13:02 +0800

    hello world

--- app/root.tsx
+++ app/root.tsx
@@ -1,30 +1,14 @@
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration
-} from "remix";
-import type { MetaFunction } from "remix";
-
-export const meta: MetaFunction = () => {
-  return { title: "New Remix App" };
-};
+import { LiveReload, Meta } from "remix";
 
 export default function App() {
   return (
-    <html lang="en">
+    <html>
       <head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <Meta />
-        <Links />
+        <title>App</title>
       </head>
       <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
+        <h1>Hello World</h1>
         {process.env.NODE_ENV === "development" && <LiveReload />}
       </body>
     </html>
