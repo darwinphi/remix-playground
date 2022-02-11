d710e0558f0aa0e3d030f54c21ab0e47d7d79c3b
Darwin Manalo <darwinmanalophi@gmail.com>
2022-02-11 20:26:30 +0800

    Add outlet to jokes

--- app/routes/jokes.tsx
+++ app/routes/jokes.tsx
@@ -1,7 +1,10 @@
+import { Outlet } from "remix";
+
 export default function Jokes() {
   return (
     <div>
       <h1>Jokes</h1>
+      <Outlet />
     </div>
   );
 }
---
+++ app/routes/jokes/index.tsx
@@ -0,0 +1,10 @@
+export default function Jokes() {
+  return (
+    <div>
+      <h2>
+        Hey girl are you a school? Because I want to shoot some kids up inside
+        of you.
+      </h2>
+    </div>
+  );
+}
